e2efe466851780f6fd988b5ef93257910f2066ea
Rangel Cortez Bacher <rcbacher@gmail.com>
2024-01-30 10:24:52 -0300

    alteração main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // Configuração de CORS
+    const corsOptions: CorsOptions = {
+      origin: 'http://localhost:3001', // Ou a origem do seu aplicativo React
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type,Authorization',
+    };
+    app.enableCors(corsOptions);  
 
   const config = new DocumentBuilder()
     .setTitle('API Intelbras')
